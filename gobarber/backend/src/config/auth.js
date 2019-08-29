90f22ba4da44b57f8e91dac74c4ef6db3a749d3d
Artur FF Santos <35586370+arturffsantos@users.noreply.github.com>
2019-08-29 00:12:41 -0300

    add SessionController, authConfig and dotenv

---
+++ gobarber/backend/src/config/auth.js
@@ -0,0 +1,4 @@
+export default {
+  secret: process.env.MD5_HASH,
+  expiresIn: '7d',
+};
--- gobarber/backend/src/server.js
+++ gobarber/backend/src/server.js
@@ -1,3 +1,5 @@
+// eslint-disable-next-line no-unused-vars
+import config from 'dotenv/config';
 import app from './app';
 
-app.listen(3333);
+app.listen(process.env.SERVER_PORT);
