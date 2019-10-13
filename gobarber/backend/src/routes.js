ed48bee444b664ad30fc01e537ed69b369e9e714
Artur FF Santos <35586370+arturffsantos@users.noreply.github.com>
2019-10-13 00:19:20 -0300

    feat(provider): add listing of providers at route get /providers

--- gobarber/backend/src/routes.js
+++ gobarber/backend/src/routes.js
@@ -2,9 +2,11 @@ import { Router } from 'express';
 import multer from 'multer';
 import multerConfig from './config/multer';
 import authMiddleware from './app/middlewares/auth';
+
 import UserController from './app/controllers/UserController';
 import SessionController from './app/controllers/SessionController';
 import FileController from './app/controllers/FileController';
+import ProviderController from './app/controllers/ProviderController';
 
 const routes = new Router();
 const upload = multer(multerConfig);
@@ -17,5 +19,7 @@ routes.use(authMiddleware);
 routes.put('/users', UserController.update);
 
 routes.post('/files', upload.single('file'), FileController.store);
+
+routes.get('/providers', ProviderController.index);
 
 export default routes;
