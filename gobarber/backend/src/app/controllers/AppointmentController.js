09ea9ce03af1c5f502c7ff1fb3f735b35df1b3da
Artur FF Santos <35586370+arturffsantos@users.noreply.github.com>
2019-10-17 00:29:19 -0300

    improvement(appointmentcontroller): validate date in store method

--- gobarber/backend/src/app/controllers/AppointmentController.js
+++ gobarber/backend/src/app/controllers/AppointmentController.js
@@ -1,4 +1,5 @@
 import * as Yup from 'yup';
+import { startOfHour, parseISO, isBefore } from 'date-fns';
 import Appointment from '../models/Appointment';
 import User from '../models/User';
 
@@ -25,10 +26,32 @@ class AppointmentController {
         .json({ error: 'You can only create appointments with providers' });
     }
 
+    // Check for past dates
+    const hourStart = startOfHour(parseISO(date));
+
+    if (isBefore(hourStart, new Date())) {
+      return res.status(400).json({ error: 'Past dates are not permitted' });
+    }
+
+    // Check date availability
+    const checkNotAvailable = await Appointment.findOne({
+      where: {
+        provider_id,
+        canceled_at: null,
+        date: hourStart,
+      },
+    });
+
+    if (checkNotAvailable) {
+      return res
+        .status(400)
+        .json({ error: 'Appointment date is not available' });
+    }
+
     const appointment = await Appointment.create({
       user_id: req.userId,
       provider_id,
-      date,
+      date: hourStart,
     });
 
     return res.send(201).json(appointment);
