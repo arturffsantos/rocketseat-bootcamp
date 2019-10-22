301fc88d7a2538e60b1d5b1854139a691761a40e
Artur FF Santos <35586370+arturffsantos@users.noreply.github.com>
2019-10-22 00:59:33 -0300

    feat(notificationcontroller): set notification as read

--- gobarber/backend/src/database/index.js
+++ gobarber/backend/src/database/index.js
@@ -24,7 +24,7 @@ class Database {
   mongo() {
     this.mongoConnection = mongoose.connect(process.env.MONGO_URL, {
       useNewUrlParser: true,
-      useFindAndModify: true,
+      useFindAndModify: false,
       useUnifiedTopology: true,
     });
   }
