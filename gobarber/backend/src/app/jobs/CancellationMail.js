158e2528bed156872293c0763b492d12c78aee37
Artur FF Santos <35586370+arturffsantos@users.noreply.github.com>
2019-10-23 20:16:05 -0300

    fix(send email): fix loading enviroments variables in Queue.js

--- gobarber/backend/src/app/jobs/CancellationMail.js
+++ gobarber/backend/src/app/jobs/CancellationMail.js
@@ -10,22 +10,27 @@ class CancellationMail {
   async handle({ data }) {
     const { appointment } = data;
 
-    await Mail.sendMail({
-      to: `${appointment.provider.name} <${appointment.provider.email}>`,
-      subject: 'Agendamento cancelado',
-      template: 'cancellation',
-      context: {
-        provider: appointment.provider.name,
-        user: appointment.user.name,
-        date: format(
-          parseISO(appointment.date),
-          "'dia' dd 'de' MMMM', às' H:mm'h'",
-          {
-            locale: pt,
-          }
-        ),
-      },
-    });
+    try {
+      const info = await Mail.sendMail({
+        to: `${appointment.provider.name} <${appointment.provider.email}>`,
+        subject: 'Agendamento cancelado',
+        template: 'cancellation',
+        context: {
+          provider: appointment.provider.name,
+          user: appointment.user.name,
+          date: format(
+            parseISO(appointment.date),
+            "'dia' dd 'de' MMMM', às' H:mm'h'",
+            {
+              locale: pt,
+            }
+          ),
+        },
+      });
+      return Promise.resolve(info);
+    } catch (err) {
+      return Promise.reject(err);
+    }
   }
 }
 
