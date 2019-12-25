d19b6c40aa397efbb8c5fee442aa0ec57b2bb544
Artur FF Santos <35586370+arturffsantos@users.noreply.github.com>
2019-12-25 12:38:59 -0300

    improvement(user controller): include avatar on return from udpdate

--- gobarber/backend/src/app/controllers/UserController.js
+++ gobarber/backend/src/app/controllers/UserController.js
@@ -1,5 +1,6 @@
 import * as Yup from 'yup';
 import User from '../models/User';
+import File from '../models/File';
 
 class UserController {
   async store(req, res) {
@@ -64,9 +65,22 @@ class UserController {
       return res.status(401).json({ error: 'Password does not match!' });
     }
 
-    const { id, name, email, provider } = await user.update(req.body);
+    await user.update(req.body);
 
-    return res.json({ id, name, email, provider });
+    const { id, name, avatar, email, provider } = await User.findByPk(
+      req.userId,
+      {
+        include: [
+          {
+            model: File,
+            as: 'avatar',
+            attributes: ['id', 'path', 'url'],
+          },
+        ],
+      }
+    );
+
+    return res.json({ id, name, email, provider, avatar });
   }
 }
 
