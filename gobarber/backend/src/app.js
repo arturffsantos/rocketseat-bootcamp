61b45cccf55a2b89b48fa0ad6c403b5cf9b0e90f
Artur FF Santos <35586370+arturffsantos@users.noreply.github.com>
2019-12-25 12:23:30 -0300

    feat(cors): enable cors

--- gobarber/backend/src/app.js
+++ gobarber/backend/src/app.js
@@ -1,5 +1,6 @@
 import 'dotenv/config';
 import express from 'express';
+import cors from 'cors';
 import * as Sentry from '@sentry/node';
 import Youch from 'youch';
 import { resolve } from 'path';
@@ -22,6 +23,7 @@ class App {
 
   middlewares() {
     this.server.use(Sentry.Handlers.requestHandler());
+    this.server.use(cors());
     this.server.use(express.json());
     this.server.use(
       '/files',
