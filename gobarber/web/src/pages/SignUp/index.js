b33fa97fddfcafea7dede153262d229167462402
Artur FF Santos <35586370+arturffsantos@users.noreply.github.com>
2020-01-05 16:13:23 -0300

    feat(@web:signin): add styles to sigin and signup

--- gobarber/web/src/pages/SignUp/index.js
+++ gobarber/web/src/pages/SignUp/index.js
@@ -1,7 +1,20 @@
 import React from 'react';
-
-// import { Container } from './styles';
+import { Link } from 'react-router-dom';
+import logo from '~/assets/logo.svg';
 
 export default function SignUp() {
-  return <div />;
+  return (
+    <>
+      <img src={logo} alt="GoBarber" />
+
+      <form>
+        <input placeholder="Nome completo" />
+        <input type="email" placeholder="Seu email" />
+        <input type="password" placeholder="Sua senha secreta" />
+
+        <button type="submit">Criar conta</button>
+        <Link to="/">Já tenho login</Link>
+      </form>
+    </>
+  );
 }
