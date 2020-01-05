510d682c95b1c43d292fc8f03f292f48a349c216
Artur FF Santos <35586370+arturffsantos@users.noreply.github.com>
2020-01-05 16:29:58 -0300

    improvement(@web:signin): add validation

--- gobarber/web/src/pages/SignUp/index.js
+++ gobarber/web/src/pages/SignUp/index.js
@@ -1,7 +1,17 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 import { Form, Input } from '@rocketseat/unform';
+import * as Yup from 'yup';
+
 import logo from '~/assets/logo.svg';
+
+const schema = Yup.object().shape({
+  name: Yup.string().required('Nome é obrigatório');
+  email: Yup.string()
+    .email('Insira um email válido')
+    .required('O email é obrigatório'),
+  password: Yup.string().min(6, 'Mínimo de 6 caracteres').required('Senha é obrigatória'),
+});
 
 export default function SignUp() {
   function handleSubmit(data) {
@@ -12,7 +22,7 @@ export default function SignUp() {
     <>
       <img src={logo} alt="GoBarber" />
 
-      <Form onSubmit={handleSubmit}>
+      <Form schema={schema} onSubmit={handleSubmit}>
         <Input name="name" placeholder="Nome completo" />
         <Input name="email" type="email" placeholder="Seu email" />
         <Input
