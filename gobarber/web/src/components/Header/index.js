e21881c82fbff65ed04835ab3f595473bf5f34b6
Artur FF Santos <35586370+arturffsantos@users.noreply.github.com>
2020-01-06 23:07:39 -0300

    feat(@web:notifications): add styles to notifications

--- gobarber/web/src/components/Header/index.js
+++ gobarber/web/src/components/Header/index.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import Notifications from '~/components/Notifications';
 import logo from '~/assets/logo-purple.svg';
 import { Container, Content, Profile } from './styles';
 
@@ -12,6 +13,7 @@ export default function Header() {
           <Link to="/dashboard">DASHBOARD</Link>
         </nav>
         <aside>
+          <Notifications />
           <Profile>
             <div>
               <strong>Mock usuario</strong>
