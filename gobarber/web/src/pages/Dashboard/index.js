b71d04ad36c98fc62abf5d697903a73950def0a8
Artur FF Santos <35586370+arturffsantos@users.noreply.github.com>
2020-01-08 22:40:43 -0300

    feat(@web:dashboard): add styles to dashboard

--- gobarber/web/src/pages/Dashboard/index.js
+++ gobarber/web/src/pages/Dashboard/index.js
@@ -1,7 +1,40 @@
 import React from 'react';
+import { MdChevronLeft, MdChevronRight } from 'react-icons/md';
+import api from '~/services/api';
 
-// import { Container } from './styles';
+import { Container, Time } from './styles';
 
 export default function Dashboard() {
-  return <h1>Dashboard</h1>;
+  return (
+    <Container>
+      <header>
+        <button type="button">
+          <MdChevronLeft size={36} color="#fff" />
+        </button>
+        <strong>08 de Janeiro</strong>
+        <button type="button">
+          <MdChevronRight size={36} color="#fff" />
+        </button>
+      </header>
+
+      <ul>
+        <Time past>
+          <strong>08:00</strong>
+          <span>Diego</span>
+        </Time>
+        <Time available>
+          <strong>09:00</strong>
+          <span>Em aberto</span>
+        </Time>
+        <Time>
+          <strong>10:00</strong>
+          <span>Diego</span>
+        </Time>
+        <Time>
+          <strong>11:00</strong>
+          <span>Diego</span>
+        </Time>
+      </ul>
+    </Container>
+  );
 }
