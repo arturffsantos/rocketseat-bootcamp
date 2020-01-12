48ebbc17c17b86f46adac61686d876d86e56464f
Artur FF Santos <35586370+arturffsantos@users.noreply.github.com>
2020-01-12 17:11:49 -0300

    improvement(@api:appointment): using cache to listing of appointments for user and page

--- gobarber/backend/src/app/controllers/AppointmentController.js
+++ gobarber/backend/src/app/controllers/AppointmentController.js
@@ -1,6 +1,7 @@
 import Appointment from '../models/Appointment';
 import User from '../models/User';
 import File from '../models/File';
+import Cache from '../../lib/Cache';
 
 import CreateAppointmentService from '../services/CreateAppointmentService';
 import CancelAppointmentService from '../services/CancelAppointmentService';
@@ -8,6 +9,13 @@ import CancelAppointmentService from '../services/CancelAppointmentService';
 class AppointmentController {
   async index(req, res) {
     const { page = 1 } = req.query;
+
+    const cachedKey = `user:${req.userId}:appointments:${page}`;
+    const cached = await Cache.get(cachedKey);
+
+    if (cached) {
+      return res.json(cached);
+    }
 
     const appointments = await Appointment.findAll({
       where: { user_id: req.userId, canceled_at: null },
@@ -30,6 +38,8 @@ class AppointmentController {
         },
       ],
     });
+
+    Cache.set(cachedKey, appointments);
 
     return res.json(appointments);
   }
