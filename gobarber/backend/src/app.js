c1be60e9392e45d80f9454bd084c587fc2a11c68
Artur FF Santos <35586370+arturffsantos@users.noreply.github.com>
2020-02-06 20:25:22 -0300

    improvement(@api:security): add helmet to improve security by setting HTTP response headers

--- gobarber/backend/src/app.js
+++ gobarber/backend/src/app.js
@@ -1,6 +1,7 @@
 import 'dotenv/config';
 import express from 'express';
 import cors from 'cors';
+import helmet from 'helmet';
 import * as Sentry from '@sentry/node';
 import Youch from 'youch';
 import { resolve } from 'path';
@@ -23,6 +24,7 @@ class App {
 
   middlewares() {
     this.server.use(Sentry.Handlers.requestHandler());
+    this.server.use(helmet());
     this.server.use(cors());
     this.server.use(express.json());
     this.server.use(
