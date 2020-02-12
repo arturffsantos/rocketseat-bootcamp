6af05f0a270153b81964824871bca663a72fc061
Artur FF Santos <35586370+arturffsantos@users.noreply.github.com>
2020-02-11 23:24:05 -0300

    fix(@api:tests): fix configs to use sqlite on tests, jest in eslint and supertest instead of axios

--- gobarber/backend/src/config/database.js
+++ gobarber/backend/src/config/database.js
@@ -1,11 +1,14 @@
-require('dotenv/config');
+/* eslint-disable no-console */
+require('../bootstrap');
 
 module.exports = {
-  dialect: 'postgres',
+  dialect: process.env.DB_DIALECT || 'postgres',
   host: process.env.DB_HOST,
   username: process.env.DB_USER,
   password: process.env.DB_PASS,
   database: process.env.DB_NAME,
+  storage: './__tests__/database.sqlite',
+  logging: process.env.DB_LOG_QUERY !== 'false' ? console.log : false,
   define: {
     timestamp: true,
     underscored: true,
